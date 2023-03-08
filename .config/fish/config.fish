7c10eaa7658355bb3d6cfe21dc5f40558d82497a
Evgenii <pitohuip@yahoo.com>
2023-03-08 20:13:19 +0500

    update doom emacs config & add .emacs.d/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+# set -x PATH "$HOME/.emacs.d/bin:$PATH"
+set -U fish_user_paths $HOME/.emacs.d/bin $fish_user_paths
 # bare git repo alias for dotfiles
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
